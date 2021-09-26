1ae0b4350db53e45a9d7b4e05bd6e59892ab5fc3
Sudam Yasodya <sudam19961994@gmail.com>
2021-09-26 18:58:59 +0530

    testing-calculator-functions

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,2 +1,38 @@
 var calculator = require('../calculator');
 
+// beforeEach(() => {
+//     console.log("Before Each");
+// });
+
+// afterAll(() => {
+//     console.log("After all");
+// });
+
+// test("two plus two is four", () => {
+//     console.log("Test 01");
+// });
+
+// test("two plus two is four", () => {
+//     console.log("Test 02");
+// });
+
+// test("two plus two is four", () => {
+//     console.log("Test 03");
+// });
+
+test("add 1 and 2 and return 3", () => {
+    expect(calculator.add(1,2)).toBe(3);
+});
+
+test("substract 2 from 3 and return 1", () => {
+    expect(calculator.subtract(3,2)).toBe(1);
+});
+
+test("multiply 2 and 3 and return 6", () => {
+    expect(calculator.multiply(2,3)).toBe(6);
+});
+
+test("divide 6 by 3 and return 2", () => {
+    expect(calculator.division(6,3)).toBe(2);
+});
+
--- tests/wordcount.test.js
+++ tests/wordcount.test.js
@@ -1,13 +1,13 @@
-var wordCount = require('../wordCount');
+// var wordCount = require('../wordCount');
 
-test('returns word count 3', ()=>{
-    expect(wordCount.uniqueWordCount("Unit Test Example")).toBe(3);
-});
+// test('returns word count 3', ()=>{
+//     expect(wordCount.uniqueWordCount("Unit Test Example")).toBe(3);
+// });
 
-test('returns word count 3', ()=>{
-    expect(wordCount.uniqueWordCount()).toBe(0);
-});
+// test('returns word count 3', ()=>{
+//     expect(wordCount.uniqueWordCount()).toBe(0);
+// });
 
-test('returns word count 3', ()=>{
-    expect(wordCount.uniqueWordCount('')).toBe(0);
-});
+// test('returns word count 3', ()=>{
+//     expect(wordCount.uniqueWordCount('')).toBe(0);
+// });
