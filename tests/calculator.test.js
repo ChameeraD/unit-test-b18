70172fca866d111369aa3026c4b36003b5d9d542
Chaminilakeesha <chaminilakeesha@gmail.com>
2021-09-26 19:04:22 +0530

    Testing_calculator

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,2 +1,17 @@
 var calculator = require('../calculator');
 
+test('adds 1 and 2 and returns 3', ()=>{
+    expect(calculator.add(1,2)).toBe(3);
+});
+
+test('substract 5 from 10 and returns 5', ()=>{
+    expect(calculator.subtract(10,5)).toBe(4);
+});
+
+test('multiply 5 by 3 and returns 15', ()=>{
+    expect(calculator.multiply(5,3)).toBe(15);
+});
+
+test('divide 10 by 5 and returns 3', ()=>{
+    expect(calculator.division(10,5)).toBe(2);
+});
