ae77f45c70d4a0d4f8df65fc0379a7fe9494bb6e
Chaminilakeesha <chaminilakeesha@gmail.com>
2021-09-26 19:09:10 +0530

    Testing

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -5,13 +5,13 @@ test('adds 1 and 2 and returns 3', ()=>{
 });
 
 test('substract 5 from 10 and returns 5', ()=>{
-    expect(calculator.subtract(10,5)).toBe(4);
+    expect(calculator.subtract(10,5)).toBe(5);
 });
 
 test('multiply 5 by 3 and returns 15', ()=>{
     expect(calculator.multiply(5,3)).toBe(15);
 });
 
-test('divide 10 by 5 and returns 3', ()=>{
+test('divide 10 by 5 and returns 2', ()=>{
     expect(calculator.division(10,5)).toBe(2);
 });
