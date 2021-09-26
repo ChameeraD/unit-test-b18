c86ad49e69ce3d2dfee67d4462790733a546e644
Shaja Nasir <Shajanasir@gmail.com>
2021-09-26 18:56:43 +0530

    add test cases

--- tests/calculator.test.js
+++ tests/calculator.test.js
@@ -1,2 +1,20 @@
-var calculator = require('../calculator');
+// Importing the file
+var calculator = require("../calculator");
 
+describe("Calculator tests", () => {
+  test("add 1 and 2 should return 3", () => {
+    expect(calculator.add(1, 2)).toBe(3);
+  });
+
+  test("Subract 5 and 3 should return 2", () => {
+    expect(calculator.subtract(5, 3)).toBe(2);
+  });
+
+  test("multiply 1 and 2 should return 2", () => {
+    expect(calculator.multiply(1, 2)).toBe(2);
+  });
+
+  test("Divide 6 and 2 should return 3", () => {
+    expect(calculator.division(6, 2)).toBe(3);
+  });
+});
